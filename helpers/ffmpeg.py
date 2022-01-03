313ccc25b17b052267d70e116157e5adcc61b1a8
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-01-03 14:29:49 +0530

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -15,14 +15,14 @@ async def extract_audio(client, message, data):
     if data['name'] == "mp3":
         out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -map 0:{data['map']} -c copy '{out_loc}' -y")
         if rcode != 0:
-            await message.edit_text("**Error Occured. See Logs for more info.**")
+            await message.edit_text("**Error Occured. Contact @pyrogrammers chat.**")
             print(err)
             await clean_up(dwld_loc, out_loc)
             return
     else:
         out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -map 0:{data['map']} '{out_loc}' -y")
         if rcode != 0:
-            await message.edit_text("**Error Occured. See Logs for more info.**")
+            await message.edit_text("**Error Occured. Contact @pyrogrammers chat.**")
             print(err)
             await clean_up(dwld_loc, out_loc)
             return
