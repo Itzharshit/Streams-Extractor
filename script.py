a488c45f2da8cd8701f1c572c27852492da03def
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-03-02 18:16:09 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,34 +5,22 @@
 
 class Script(object):
 
-    START_MSG = """𝗛𝗶𝗶 {},
+    START_MSG = """Hii {},
 
-𝗜 𝗮𝗺 𝘀𝘁𝗿𝗲𝗮𝗺 𝗲𝘅𝘁𝗿𝗮𝗰𝘁𝗼𝗿 𝗯𝗼𝘁 𝗰𝗿𝗲𝗮𝘁𝗲𝗱 𝗯𝘆  𝗶 𝗰𝗮𝗻 𝗲𝘅𝘁𝗿𝗮𝗰𝘁 𝗮𝘂𝗱𝗶𝗼 𝗮𝗻𝗱 𝘀𝘂𝗯𝘁𝗶𝘁𝗹𝗲𝘀 𝗳𝗿𝗼𝗺 𝘃𝗶𝗱𝗲𝗼.
-
-𝗦𝗲𝗻𝗱 𝗺𝗲 𝘃𝗶𝗱𝗲𝗼 𝗳𝗶𝗹𝗲 𝘁𝗼 𝗰𝗼𝗻𝘁𝗶𝗻𝘂𝗲.
-
-See <i>help</i> for more details.
-
-© @Pyrogrammers</b>"""
+I am Stream Extractor bot, I can extract audio and subtitles from any video file./nHit /help to know more."""
 
 
-    HELP_MSG = """<b>Hii, Follow these Steps..</b>
-
-🌀 <i>Send me any Valid video file.</i>
-
-🌀 <i>Click Download and Process button to download the file to my server</i>
-
-🌀 <i>Wait untill I process the video!</i>
-
-🌀 <i>Now select the stream you need to extract!</i>"""
+    HELP_MSG = """<b>You can follow below steps to use me.</b>
+<i>Send me any video file.</i>
+<i>Click Download and Process button to download the file.</i>
+<i>Wait untill processing the video!</i>
+<i>After that select the stream you need to extract!</i>"""
 
 
     ABOUT_MSG = """<b>Streams Extractor Bot</b>
 
 <b>Creater :</b> @pyrogrammers    
-
 <b>Language :</b> <code>Python3</code>
-
+<b>YouTube :</b> <a href='https://youtube.com/channel/UC2anvk7MNeNzJ6B4c0SZepw'>YouTube Channel</a> 
 <b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
 """
