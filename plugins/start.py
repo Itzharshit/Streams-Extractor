ea460846d94428745d99c0d496586e6f9e4e766e
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-03-05 00:50:00 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,10 +14,8 @@ from script import Script
 @trojanz.on_message(filters.command(["start"]) & filters.private)
 async def start(client, message):
     await message.reply_photo(
-        chat_id = message.from_user.id
         photo="https://i.ibb.co/NKXgXD4/vlmnwosn-0.png",
         caption=Script.START_MSG.format(message.from_user.mention),
-        disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
